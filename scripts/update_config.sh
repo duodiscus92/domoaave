96b91304361a1521405becb2738a376a3712f68b
duodiscus92 <jacques.ehrlich@orange.fr>
2026-04-02 18:58:07 +0200

    Réorganisation du projet : src/include/infra, adaptation CMake et scripts

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# ce script est appelé par cron vers minuit
+
+# Charger config locale
+source /opt/domoaave/config_local.sh
+
 #SERVER="jehrlich@192.168.1.44"
-SERVER="jehrlich@marconi.local"
-KEY="/home/jehrlich/.ssh/id_ed25519_domoticz"
-SRC="/home/jehrlich/domoaave/config/"
-DST="/opt/domoaave/"
-#scp -i "$KEY" ${SERVER}:${SRC}*.json "$DST"
+#KEY="/home/jehrlich/.ssh/id_ed25519_domoticz"
+#SRC="/home/jehrlich/domoaave/config/"
+#DST="/opt/domoaave/"
 rsync -avz -e "ssh -i $KEY" ${SERVER}:${SRC} "$DST"
